9e4b3d3f9fac394c7bc518e32bac77b7634641e3
sam <samuelwoo.au@gmail.com>
2017-10-11 23:24:42 +1100

    refactor on each (#6)

--- unit_test/each_test.go
+++ unit_test/each_test.go
@@ -8,9 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type _eachTester struct{}
+type eachTester struct{}
 
-func (anonymous *_eachTester) testWithError(t *testing.T) {
+func (anonymous *eachTester) testWithError(t *testing.T) {
 	// Type mismatched then do nothing
 	input := []int64{1, 2, 3, 4, 5, 6, 7, 8, 99}
 	callCount := 0
@@ -51,7 +51,7 @@ func (anonymous *_eachTester) testWithError(t *testing.T) {
 	assert.Equal(t, 0, callCount)
 }
 
-func (anonymous *_eachTester) testWithArray(t *testing.T) {
+func (anonymous *eachTester) testWithArray(t *testing.T) {
 	callCount := 0
 	input := [9]int64{1, 2, 3, 4, 5, 6, 7, 8, 99}
 	mutex := &sync.Mutex{}
@@ -64,8 +64,11 @@ func (anonymous *_eachTester) testWithArray(t *testing.T) {
 	assert.Equal(t, len(input), callCount)
 }
 
-func (anonymous *_eachTester) testWithSlice(t *testing.T) {
-	input := []int64{1, 2, 3, 4, 5, 6, 7, 8, 99}
+func (anonymous *eachTester) testWithSlice(t *testing.T) {
+	input := []int64{}
+	for i := 0; i < 999999; i++ {
+		input = append(input, int64(i))
+	}
 	callCount := 0
 	mutex := &sync.Mutex{}
 	piezas.Each(input,
@@ -78,7 +81,7 @@ func (anonymous *_eachTester) testWithSlice(t *testing.T) {
 }
 
 func TestEach(t *testing.T) {
-	anonymous := _eachTester{}
+	anonymous := eachTester{}
 	anonymous.testWithError(t)
 	anonymous.testWithSlice(t)
 	anonymous.testWithArray(t)
