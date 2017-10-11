c0023fd98045b04d91b73840813ac43af2700f57
samwooo <samuelwoo.au@gmail.com>
2017-10-12 03:33:19 +1100

    a refactor on waterfall

--- unit_test/waterfall_test.go
+++ unit_test/waterfall_test.go
@@ -13,21 +13,23 @@ type _waterfallTester struct{}
 
 func (anonymous *_waterfallTester) testWithError(t *testing.T) {
 	// Error would NOT stop the waterfall but will be piped all the way down to the end ! ?
-	piezas.NewWaterfall(piezas.Tasks{
+	r := piezas.NewWaterfall(piezas.Tasks{
 		func(a ...int) (int, int, error) {
 			return 0, 0, fmt.Errorf("Lucy&Lily")
 		},
 		func(a int, b int, e error) (int, error) {
 			return a + b, e
 		},
-		func(a int, e error) {
+		func(a int, e error) (int, error) {
 			assert.Equal(t, 0, a)
 			assert.Equal(t, "Lucy&Lily", e.Error())
+			return a, e
 		}}, 1, 2, 3)
+	assert.Equal(t, []interface{}{0, fmt.Errorf("Lucy&Lily")}, r)
 }
 
 func (anonymous *_waterfallTester) testWithVariadicParas(t *testing.T) {
-	piezas.NewWaterfall(piezas.Tasks{
+	r := piezas.NewWaterfall(piezas.Tasks{
 		func(a ...int) (int, int, error) {
 			total := 1
 			count := 0
@@ -40,47 +42,55 @@ func (anonymous *_waterfallTester) testWithVariadicParas(t *testing.T) {
 		func(a int, b int, e error) int {
 			return a + b
 		},
-		func(a int) {
+		func(a int) int {
 			assert.Equal(t, 362925, a)
+			return a
 		}}, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	assert.Equal(t, []interface{}{362925}, r)
 }
 
 func (anonymous *_waterfallTester) testWithFixedParas(t *testing.T) {
-	piezas.NewWaterfall(piezas.Tasks{
+	r := piezas.NewWaterfall(piezas.Tasks{
 		func(a int) (int, string, error) {
 			return a * a, strconv.Itoa(a), nil
 		},
 		func(a int, b string, e error) string {
 			return strconv.Itoa(a) + b
 		},
-		func(a string) {
+		func(a string) string {
 			assert.Equal(t, "10000100", a)
+			return a
 		}}, 100)
+	assert.Equal(t, []interface{}{"10000100"}, r)
 }
 
 func (anonymous *_waterfallTester) testWithCombination(t *testing.T) {
-	piezas.NewWaterfall(piezas.Tasks{
+	r := piezas.NewWaterfall(piezas.Tasks{
 		func(a int) (int, string, error) {
 			return a * a, strconv.Itoa(a), nil
 		},
 		func(a int, b string, e error) string {
 			return strconv.Itoa(a) + b
 		},
-		func(a string) {
+		func(a string) string {
 			assert.Equal(t, "10000100", a)
 
-			piezas.NewWaterfall(piezas.Tasks{
+			r := piezas.NewWaterfall(piezas.Tasks{
 				func(a string) (string, error) {
 					return a + a, nil
 				},
 				func(b string, e error) (int, error) {
 					return strconv.Atoi(b)
 				},
-				func(a int, e error) {
+				func(a int, e error) (int, error) {
 					assert.Equal(t, nil, e)
 					assert.Equal(t, 1000010010000100, a)
+					return a, e
 				}}, a)
+			assert.Equal(t, []interface{}{1000010010000100, nil}, r)
+			return a
 		}}, 100)
+	assert.Equal(t, []interface{}{"10000100"}, r)
 }
 
 func TestWaterfall(t *testing.T) {
